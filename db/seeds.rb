80e569d69f10c40e13d62223b46d4ad6e325cde0
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-24 09:33:30 -0600

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+DishIngredient.destroy_all
+Ingredient.destroy_all
+Dish.destroy_all
+Chef.destroy_all
+
+sir_shak = Chef.create!(name: "Sir Shak Shuka")
+shakshuka = sir_shak.dishes.create!(name: "Shakshuka", description: "Eggs, tomatoes, and all the spiced goodness you could imagine.")
+eggs = Ingredient.create!(name: "Eggs", calories: 90)
+tomatoes = Ingredient.create!(name: "Tomatoes", calories: 50)
+onions = Ingredient.create!(name: "Onions", calories: 30)
+shakshuka_eggs = DishIngredient.create!(dish_id: shakshuka.id, ingredient_id: eggs.id)
+shakshuka_tomatoes = DishIngredient.create!(dish_id: shakshuka.id, ingredient_id: tomatoes.id)
+shakshuka_onions = DishIngredient.create!(dish_id: shakshuka.id, ingredient_id: onions.id)
